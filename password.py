ed787e7b1ca934299f8ea241c2e4564cd3792f9b
Oduor Mike <87503468+ohmed@users.noreply.github.com>
2025-02-18 21:41:30 +0300

    Added the python password strength checker file

---
+++ password.py
@@ -0,0 +1,37 @@
+import re
+
+def check_password_strength(password):
+	length_error = len(password) < 8
+	uppercase_error = not any(char.isupper() for char in password)
+	lowercase_error = not any(char.islower() for char in password)
+	digit_error = not any(char.isdigit() for char in password)
+
+
+
+	errors = {
+		"Too short (min 8 characters)": length_error,
+		"Must contain at least one uppercase": uppercase_error,
+		"Must contain at least one lowercase": lowercase_error,
+		"Must contain at least one number": digit_error,
+	}
+
+	if any(errors.values()):
+		print("Weak password! Issues:")
+		for issue, exists in errors.items():
+			if exists:
+				print(f"- {issue}")
+	
+	else:
+		print("Strong Password!")
+
+
+if __name__ == "__main__":
+
+	while True:
+		user_password = input("Enter a password (or type 'exit' to quit): ")
+		if user_password .lower() == 'exit':
+			print("Exiting the script. ")
+			break
+
+
+		check_password_strength(user_password)
